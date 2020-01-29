d3dcbd77896868be234bf821d1b06558a6141da4
Jeremy Lanssiers <10577835+jlanssie@users.noreply.github.com>
2020-01-29 23:19:02 +0100

    Update java.sh

--- java.sh
+++ java.sh
@@ -5,3 +5,28 @@ function javaclocal {
 function javacbin {
     javac -d bin $(find . -name "*.java")
 }
+
+function javacfxlocal {
+    javac --module-path /usr/lib/jvm/javafx/javafx-sdk-13.0.2/lib --add-modules javafx.controls -d bin $(find . -name "*.java")
+}
+
+function javacfxbin {
+    javac --module-path /usr/lib/jvm/javafx/javafx-sdk-13.0.2/lib --add-modules javafx.controls -d bin $(find . -name "*.java")
+}
+
+function javacfx {
+    CLASS=""
+
+    if [ "$#" -eq 1 ]
+    then
+        CLASS="$1"
+    fi
+
+    if [ "$#" -gt 1 ]
+    then
+        echo "Input 0 or 1 parameters"
+        return 1 2>/dev/null
+    fi
+
+    java --module-path /usr/lib/jvm/javafx/javafx-sdk-13.0.2/lib --add-modules javafx.controls $1
+}
