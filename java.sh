af572613d7f49c1a47a519d4c7bebcace87da40c
Jeremy Lanssiers <10577835+jlanssie@users.noreply.github.com>
2020-01-29 23:36:08 +0100

    Update java.sh

--- java.sh
+++ java.sh
@@ -1,17 +1,25 @@
 function javaclocal {
-    javac $(find . -name "*.java");
+	javac $(find . -name "*.java");
 }
 
 function javacbin {
-    javac -d bin $(find . -name "*.java")
+	javac -d bin $(find . -name "*.java")
+}
+
+function javacwd {
+    javac -d . $(find . -name "*.java")
 }
 
 function javacfxlocal {
-    javac --module-path /usr/lib/jvm/javafx/javafx-sdk-13.0.2/lib --add-modules javafx.controls -d bin $(find . -name "*.java")
+    javac --module-path /usr/lib/jvm/javafx/javafx-sdk-13.0.2/lib --add-modules javafx.controls $(find . -name "*.java")
 }
 
 function javacfxbin {
     javac --module-path /usr/lib/jvm/javafx/javafx-sdk-13.0.2/lib --add-modules javafx.controls -d bin $(find . -name "*.java")
+}
+
+function javacfxwd {
+    javac --module-path /usr/lib/jvm/javafx/javafx-sdk-13.0.2/lib --add-modules javafx.controls -d . $(find . -name "*.java")
 }
 
 function javacfx {
