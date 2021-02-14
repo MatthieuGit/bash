15c5c3fed02f5678948bc2b9c2d3b2bb8ff7907e
Jeremy Lanssiers <10577835+jlanssie@users.noreply.github.com>
2021-02-14 22:17:38 +0100

    Create update-ssl.sh

---
+++ update-ssl.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# FUNCTION: Updates SSL certificates
+# ARGUMENTS: none
+# AUTHOR: jlanssie@gmail.com
+# COPYRIGHT: 2021 GNU
+# VERSION: 1.0
+# REQUIRES:
+
+TARGET="/mydestination/"
+LOGFILE="log.txt"
+
+cp --remove-destination /fullchain.pem/ $TARGET
+cp --remove-destination /privkey.pem/ $TARGET
+
+chown -R user:user $TARGET
+
+echo Last update - $(date) >> $TARGET$LOGFILE
+
+exit 0
