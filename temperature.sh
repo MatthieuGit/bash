8b2e8eb753d22d7e079dd2750b8e04568bf97d90
Jeremy Lanssiers <10577835+jlanssie@users.noreply.github.com>
2021-03-13 00:37:55 +0100

    Update temperature.sh

--- temperature.sh
+++ temperature.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+# FUNCTION: Echo temperature
+# ARGUMENTS: none
+# AUTHOR: Jeremy Lanssiers
+# COPYRIGHT: 2021 GNU
+# VERSION: 1.0
+# REQUIRES: thermal sensor
+
 TEMP=$(cat /sys/class/thermal/thermal_zone0/temp)
 TEMP=$(echo "scale=2 ; $TEMP / 1000" | bc)
 echo $TEMP "°C"
