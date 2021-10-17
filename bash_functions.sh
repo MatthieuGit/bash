e83c2a681d661684f3dce34a350c3f01f664751e
jlanssie <10577835+jlanssie@users.noreply.github.com>
2021-10-17 22:21:55 +0200

    2021-10-17 -- 22:21:55

--- bash_functions.sh
+++ bash_functions.sh
@@ -1,3 +1,14 @@
+#APT
+
+function update {
+    sudo apt-get update;
+    sudo apt-get upgrade;
+    sudo apt-get autoremove --purge;
+    sudo apt-get clean;
+}
+
+#Git
+
 function gitpush {
     git add -A;
     COMMENT=""
@@ -32,16 +43,54 @@ function gitforce {
     git push --force origin $1:master; 
 }
 
+#Java
+
+function javaclocal {
+    javac $(find . -name "*.java");
+}
+
+function javacbin {
+    javac -d bin $(find . -name "*.java")
+}
+
+function javacwd {
+    javac -d . $(find . -name "*.java")
+}
+
+function javacfxlocal {
+    javac --module-path /usr/lib/jvm/javafx/javafx-sdk-13.0.2/lib --add-modules javafx.controls $(find . -name "*.java")
+}
+
+function javacfxbin {
+    javac --module-path /usr/lib/jvm/javafx/javafx-sdk-13.0.2/lib --add-modules javafx.controls -d bin $(find . -name "*.java")
+}
+
+function javacfxwd {
+    javac --module-path /usr/lib/jvm/javafx/javafx-sdk-13.0.2/lib --add-modules javafx.controls -d . $(find . -name "*.java")
+}
+
+function javafx {
+    CLASS=""
+
+    if [ "$#" -eq 1 ]
+    then
+        CLASS="$1"
+    fi
+
+    if [ "$#" -ne 1 ]
+    then
+        echo "USAGE: Input a Java class as parameter"
+        return 1 2>/dev/null
+    fi
+
+    #Set JavaFX path here
+    java --module-path /usr/lib/jvm/javafx/javafx-sdk-13.0.2/lib --add-modules javafx.controls $1
+}
+
+#eyeD3
 
 function mp3idv3 {
     find . -name *.mp3 -exec eyeD3 --to-v2.4  {} \; 
     find . -name *.mp3 -exec eyeD3 --to-v1.1  {} \; 
     find . -name *.mp3 -exec eyeD3 --remove-v1  {} \;
-}
-
-function update {
-    sudo apt-get update;
-    sudo apt-get upgrade;
-    sudo apt-get autoremove --purge;
-    sudo apt-get clean;
 }
