599d0ed502f3506f2e7dca690973a1543dc3c9fa
LAN1663 <jeremylanssiers@Glam-Jay.local>
2022-03-07 10:26:32 +0100

    2022-03-07 -- 10:26:32

--- zshrc.sh
+++ zshrc.sh
@@ -1,4 +1,13 @@
+# ---- Homebrew
+
+export PATH="/opt/homebrew/bin:$PATH"
+export PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"
+
 # ---- Prompt
 
+return_git_branch() {
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+
 autoload -U colors && colors
-PS1="%{$fg[cyan]%}%n%{$reset_color%}%{$fg[cyan]%}@%{$reset_color%}%{$fg[cyan]%}%m %{$reset_color%}%{$fg[yellow]%}%~ %{$reset_color%}% $ "
+PS1="%{$fg[cyan]%}%n%{$reset_color%}%{$fg[cyan]%}@%{$reset_color%}%{$fg[cyan]%}%m %{$reset_color%}%{$fg[yellow]%}%~$(return_git_branch) %{$reset_color%}% $ "
