82cb1625232ae667620a32d6b55d450bb405820b
Jeremy Lanssiers <jlanssie@gmail.com>
2022-05-04 21:12:14 +0200

    2022-05-04 -- 21:12:14

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,2 +1,3 @@
 alias cheatsheet="cat ~/repos/bash/cheatsheet.sh"
 alias toggle-wifi='~/repos/bash/toggle-wifi.sh'
+alias ssh-local-to-remote="ssh-keygen -t rsa; ssh-copy-id -p 1234 user@192.168.0.1"
