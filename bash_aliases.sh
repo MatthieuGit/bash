f81f12663fe08099bcc3ea1e063d978fbf339a25
jeremy.lanssiers@nationale-loterij.be <jeremy.lanssiers@nationale-loterij.be>
2022-06-01 21:19:37 +0200

    2022-06-01 -- 21:19:37

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,3 +1,2 @@
 alias cheatsheet="cat ~/repos/bash/cheatsheet.sh"
 alias toggle-wifi='~/repos/bash/toggle-wifi.sh'
-alias ssh-local-to-remote="ssh-keygen -t rsa; ssh-copy-id -p 1234 user@192.168.0.1"
