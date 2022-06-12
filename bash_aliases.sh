659a59d377e0330a5ee64079e1c72559885c065d
Jeremy Lanssiers <jlanssie@gmail.com>
2022-06-12 22:13:02 +0200

    2022-06-12 -- 22:13:02

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,3 +1,5 @@
 alias cheatsheet="cat ~/repos/bash/cheatsheet.sh";
-alias toggle-wifi='~/repos/bash/toggle-wifi.sh';
 alias myip='echo $(curl -s ifconfig.me)';
+alias toggle-wifi='~/repos/bash/toggle-wifi.sh';
+alias convert-flac-to-mp3="find . -name '*.flac' -type f -exec bash -c 'ffmpeg -i "{}" -ab 320k -map_metadata 0 -id3v2_version 3 "{}".mp3' \;
+";
--- cheatsheet.sh
+++ cheatsheet.sh
@@ -4,11 +4,11 @@
 
 exit 0
 
-# Move all files in current directory to another directory
-find . -maxdepth 1 -name '*' -type f -exec mv -t ./anotherdirectory {} +
+# Redirect standard output and standard error into a log
+mycommand >> mycommand.log 2>&1
 
-# Rename pattern 'a' to 'b' in all filenames in current directory
-find . -maxdepth 1 -name "*" -type f -exec rename -n 's/a/b/g' {} +
+# Install deb package
+sudo dpkg -i package
 
 # Find pattern 'abc' in all files in current directory
 find . -type f -exec sh -c 'cat "$1" | grep a' sh {} \;
@@ -16,26 +16,42 @@ find . -type f -exec sh -c 'cat "$1" | grep a' sh {} \;
 # Rename pattern 'a' to 'b' in all files in current directory
 find . -type f -exec sed -i '' -e 's/a/b/g' {} \;
 
-# Convert all flac files to mp3
-find . -name '*.flac' -type f -exec bash -c 'ffmpeg -i "{}" -ab 320k -map_metadata 0 -id3v2_version 3 "{}".mp3' \;
+# Rename pattern 'a' to 'b' in all filenames in current directory
+find . -maxdepth 1 -name "*" -type f -exec rename -n 's/a/b/g' {} +
 
-# Create a local SSH key and copy it to remote to allow seamless connection
-alias ssh-local-to-remote='ssh-keygen -t rsa; ssh-copy-id -p 1234 user@192.168.0.1'
+# Move all files in current directory to another directory
+find . -maxdepth 1 -name '*' -type f -exec mv -t ./anotherdirectory {} +
 
-# Create an SSH tunnel on port 1111 that takes remote input from port 2222 and tunnels to local output on port 3333
-ssh -p 1111 123.123.123.123 -L 2222:localhost:3333
+# Create a local SSH key and copy it to remote address to allow passwordless connection
+ssh-keygen -t rsa; ssh-copy-id -p 1234 user@111.111.111.111
 
-# SSH using key KEY and copy FILE from REMOTE to LOCAL
-scp -i /Users/jeremylanssiers/.ssh/KEY remote-user@123.123.123.123:/remote-path/FILE /local-path/FILE
+# Create an SSH tunnel on port 1111 that takes remote address input on port 2222 and tunnels to localhost:3333
+ssh -p 1111 111.111.111.111 -L 2222:localhost:3333
+
+# SSH using key KEY and copy FILE from remote user at remote address to local path
+scp -i /Users/jeremylanssiers/.ssh/KEY remote-user@111.111.111.111:/remote-path/FILE /local-path/FILE
+
+# Get ALPN status
+echo | openssl s_client -alpn h2 -connect localhost:1111 | grep ALPN
+
+# Get status of ports on which processes are listening
+sudo netstat -nelt | grep LISTEN
+sudo lsof -i -P -n | grep LISTEN
+
+# Get detailed status on port
+sudo lsof -i:1111
+
+# Get a map of open ports on address
+sudo nmap -sTU -O 111.111.111.111
+
+# Get details of request to address
+curl -sIv 111.111.111.111
+
+# Get details of request to address with set header
+curl -sIv 111.111.111.111 --header "X-MyHeader: 123" www.google.com
 
 # Find files larger than 100MB 
 find . -type f -size +100M -exec sh -c 'du -hs "$1"' sh {} \;
 
 # Display file size of current path and all children to a max depth
 du -h . --max-depth=5 
-
-# Redirect standard output and standard error into a log
-mycommand >> mycommand.log 2>&1
-
-# Install deb package
-sudo dpkg -i package
