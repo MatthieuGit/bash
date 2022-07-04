e52654ad803bdcaa8f6e69e663ee4e26897d2abf
Jeremy Lanssiers <jlanssie@gmail.com>
2022-07-04 21:36:52 +0200

    2022-07-04 -- 21:36:52

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,5 +1,6 @@
 alias cheatsheet="cat ~/repos/bash/cheatsheet.sh";
-alias myip='echo $(curl -s ifconfig.me)';
 alias toggle-wifi='~/repos/bash/toggle-wifi.sh';
 alias convert-flac-to-mp3="find . -name '*.flac' -type f -exec bash -c 'ffmpeg -i "{}" -ab 320k -map_metadata 0 -id3v2_version 3 "{}".mp3' \;
 ";
+alias ppa-ls="grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/*"
+alias ip-ls="alias myip='echo $(curl -s ifconfig.me)';"
