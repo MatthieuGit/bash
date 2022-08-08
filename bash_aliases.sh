3c8b5131e477d955cb2d20694c27f1a0af9555dc
jeremy.lanssiers@nationale-loterij.be <jeremy.lanssiers@nationale-loterij.be>
2022-08-08 15:11:39 +0200

    2022-08-08 -- 15:11:39

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,4 +1,5 @@
 alias cheatsheet="cat ~/repos/bash/cheatsheet.sh";
+alias tidy="cat ~/repos/bash/tidy.sh";
 alias toggle-wifi='~/repos/bash/toggle-wifi.sh';
 alias convert-flac-to-mp3="find . -name '*.flac' -type f -exec bash -c 'ffmpeg -i "{}" -ab 320k -map_metadata 0 -id3v2_version 3 "{}".mp3' \;
 ";
