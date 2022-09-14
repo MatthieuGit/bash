b5db7e21455320b662333153cc7665d96fe77a8d
jeremy.lanssiers@nationale-loterij.be <jeremy.lanssiers@nationale-loterij.be>
2022-09-14 22:52:24 +0200

    main - update

--- tidy.sh
+++ tidy.sh
@@ -2,9 +2,9 @@
 #
 # Tidy your machine
 
-desktop_files="$HOME""/Desktop/*"
-downloads="$HOME""/Downloads/*"
-screenshots="$HOME""/Pictures/Screen*"
-MacOS_trash_files="$HOME"".Trash/*"
+desktop_files="~/Desktop/*"
+downloads="~/Downloads/*"
+screenshots="~/Pictures/Screen*"
+MacOS_trash_files="~/.Trash/*"
 
 rm -rf $(echo "$desktop_files" "$downloads" "$screenshots" "$MacOS_trash_files")
